efaeb2cabda5cc7483e9d7f4a452d74a138762da
Ata <atasayin1@gmail.com>
2022-12-02 17:49:09 +0300

    maze class

---
+++ maze_mini_game/maze.py
@@ -0,0 +1,43 @@
+class Maze:
+    def __init__(self, grid, start_point, end_point):
+        self.isWin = False
+        grid[end_point[0]][end_point[1]] = 2
+        self.grid = grid
+        self.start_point = start_point
+        self.current_point = start_point
+        self.end_point = end_point
+
+    def move(self, dv):
+        move_point = self.current_point[0] + dv[0], self.current_point[1] + dv[1]
+
+        if self.grid[move_point[0]][move_point[1]] == 0:
+            print(f"Moved from {self.current_point} to {move_point}")
+            self.current_point = move_point
+            return True
+        elif self.grid[move_point[0]][move_point[1]] == 1:
+            print(f"Cannot move from {self.current_point} to {move_point}")
+            return False
+        elif self.grid[move_point[0]][move_point[1]] == 2:
+            self.current_point = move_point
+            self.win()
+            return True
+    def win(self):
+        self.isWin = True
+        print(f"You have reached the end point at {self.current_point}")
+    
+    def hint(self):
+        pass
+    def solver(self):
+        pass
+
+
+
+
+
+
+    
+    
+
+
+
+
